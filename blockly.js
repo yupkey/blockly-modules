700b4b018b0476c0c21161be3d7672237d3cd59c
Pawel Kaczmarek <p.u.kaczmarek@gmail.com>
2019-08-12 09:15:49 +0200

    Fix build path

--- blockly.js
+++ blockly.js
@@ -1,7 +1,7 @@
-const Blockly = require('./dist/blockly_compressed');
+const Blockly = require('./build/blockly_compressed');
 
-Blockly.Msg = Object.assign(Blockly.Msg, require('./dist/i18n/en'));
-Blockly.Blocks = Object.assign(Blockly.Blocks, require('./dist/blocks_compressed')(Blockly));
-Blockly.JavaScript = require('./dist/javascript_compressed')(Blockly);
+Blockly.Msg = Object.assign(Blockly.Msg, require('./build/i18n/en'));
+Blockly.Blocks = Object.assign(Blockly.Blocks, require('./build/blocks_compressed')(Blockly));
+Blockly.JavaScript = require('./build/javascript_compressed')(Blockly);
 
 module.exports = Blockly;
